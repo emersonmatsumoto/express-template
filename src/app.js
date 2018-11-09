99021c9ef2815c90ddc89215ccb341d9ccb54aff
Emerson Matsumoto <emerson.matsumoto@pagar.me>
2018-11-09 10:54:30 -0200

    remove index route

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const cookieParser = require('cookie-parser')
 const { transports } = require('./logger')
 const expressWinston = require('express-winston')
 
-const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
 const app = express()
@@ -23,7 +22,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
 module.exports = app
