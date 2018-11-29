ec8590a6a1460e53506575d3590e512fc7b1cdc1
Emerson Matsumoto <emerson.matsumoto@pagar.me>
2018-11-29 12:18:31 -0200

    add stack in response

--- src/error-handler.js
+++ src/error-handler.js
@@ -1,7 +1,5 @@
-const logger = require('./logger')
-
 const errorHandler = ({ message, stack }, req, res, next) => {
-  logger.error(message, { stack })
+  res.stack = stack
   res.status(500).send({ error: message })
 }
 
